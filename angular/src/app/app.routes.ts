c276e9d65d600b405998ca38f78cb1bcdfb6ac00
rotelstift <rotelstift@gmail.com>
2024-03-09 21:50:45 +0900

    import design system

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 
 export const routes: Routes = [
+  { path: '', component: HelloComponent },
   { path: 'hello', component: HelloComponent }
 ];
---
+++ angular/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss"
+const config: Config = {
+  content: [
+    './src/**/*.{html, ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
