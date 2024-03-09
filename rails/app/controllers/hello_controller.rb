035d462e978c9010d91ce47fdb3a053b5c167bda
rotelstift <rotelstift@gmail.com>
2024-03-09 23:10:06 +0900

    corsのgemを有効化して古い設定を削除する

--- rails/app/controllers/hello_controller.rb
+++ rails/app/controllers/hello_controller.rb
@@ -1,7 +1,6 @@
 class HelloController < ApplicationController
   def index
     @hello = {answer: 'hello world'}
-    response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:4200'
     render json: @hello
   end
 end
