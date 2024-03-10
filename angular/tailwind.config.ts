091eef6933a0e982f90db2092dc6f12c6e54c742
rotelstift <rotelstift@gmail.com>
2024-03-11 08:34:11 +0900

    headerのデザインをもう少し調整

--- angular/tailwind.config.ts
+++ angular/tailwind.config.ts
@@ -8,7 +8,10 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'icon-menu': "url('assets/ionicons/menu.svg')",
-        'icon-close': "url('assets/ionicons/close.svg')"
+        'icon-close': "url('assets/ionicons/close.svg')",
+        'icon-person': "url('assets/ionicons/person.svg')",
+        'icon-home': "url('assets/ionicons/home.svg')",
+        'icon-color-palette': "url('assets/ionicons/color-palette.svg')",
       }
     },
   },
