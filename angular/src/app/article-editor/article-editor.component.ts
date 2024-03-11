521523263eacabb5acd07c511d1955d9101535ae
rotelstift <rotelstift@gmail.com>
2024-03-11 18:02:18 +0900

    画像を選んだ時にサムネイルが表示されるようにする

--- angular/src/app/article-editor/article-editor.component.ts
+++ angular/src/app/article-editor/article-editor.component.ts
@@ -1,14 +1,17 @@
-import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-article-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-editor.component.html',
   styleUrl: './article-editor.component.css'
 })
 export class ArticleEditorComponent {
+  image_name: string | undefined
+  image_src: string | ArrayBuffer | null = ''
 
   constructor(
     private articleService: ArticleService
@@ -28,5 +31,15 @@ export class ArticleEditorComponent {
 
   selectImage(input: HTMLInputElement): void {
     input.click()
+  }
+
+  onImageSelected(event: Event): void {
+    const image = event.currentTarget as HTMLInputElement
+    if (image.files) {
+      const reader = new FileReader()
+      reader.onload = _e => this.image_src = reader.result
+      reader.readAsDataURL(image.files[0])
+      this.image_name = image.files[0].name
+    }
   }
 }
