1dfe1f5854f31eee671f0171baa470522a7e028a
rotelstift <rotelstift@gmail.com>
2024-03-11 21:06:11 +0900

    articlesを読み込んで簡単に表示した

--- angular/src/app/article-reader/article-reader.component.ts
+++ angular/src/app/article-reader/article-reader.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticleService, Article } from '../article.service';
 
 @Component({
   selector: 'app-article-reader',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-reader.component.html',
   styleUrl: './article-reader.component.css'
 })
 export class ArticleReaderComponent {
+  articles: Article[] = []
 
+  constructor(
+    private articleService: ArticleService
+  ) {}
+
+  ngOnInit() {
+    this.articleService.getAllArticleData().subscribe({
+      next: (response) => {
+        console.log(response)
+        this.articles = response
+      },
+      error: (error) => console.error(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
