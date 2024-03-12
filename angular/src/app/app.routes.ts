7d25e4ea24487defb8d5347865f8ff177fee71c9
rotelstift <rotelstift@gmail.com>
2024-03-12 10:08:32 +0900

    galleryのPC版は横スクロールに対応した

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { ArticleEditorComponent } from './article-editor/article-editor.component';
 import { ArticleReaderComponent } from './article-reader/article-reader.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
   { path: '', component: ArticleReaderComponent },
   { path: 'hello', component: HelloComponent },
-  { path: 'article-edit', component: ArticleEditorComponent }
+  { path: 'article-edit', component: ArticleEditorComponent },
+  { path: 'gallery', component: GalleryComponent }
 ];
