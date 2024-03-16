2d6d45979f6c0a0871e9f8f9f3bdd8e4ba0a41a2
rotelstift <rotelstift@gmail.com>
2024-03-16 14:07:04 +0900

    admin用のリンクを作る

---
+++ angular/src/app/admin-link/admin-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-link.component.html',
+  styleUrl: './admin-link.component.css'
+})
+export class AdminLinkComponent {
+
+}
