cfac160542ece7cb43d88b9dd618254d9a018ba2
rotelstift <rotelstift@gmail.com>
2024-03-16 20:30:44 +0900

    init tag editor

---
+++ angular/src/app/tag-editor/tag-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag-editor.component.html',
+  styleUrl: './tag-editor.component.css'
+})
+export class TagEditorComponent {
+
+}
