2f9885d7d7ebce4725a17281bc895e08d3a20728
rotelstift <rotelstift@gmail.com>
2024-03-16 20:12:20 +0900

    validation をかける

--- rails/app/models/tag.rb
+++ rails/app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :taggings
   has_many :articles, through: :taggings
+
+  validates :slug, format: { with: /\A[a-z0-9]+\z/ }
 end
--- rails/app/models/tagging.rb
+++ rails/app/models/tagging.rb
@@ -1,4 +1,14 @@
 class Tagging < ApplicationRecord
   belongs_to :article
   belongs_to :tag
+
+  validate :tags_count_within_limit, on: :create
+
+  private
+
+  def tags_count_within_limit
+    if article.tags.count > 5
+      errors.add(:article, 'に付けられるタグの数は５つまでです')
+    end
+  end
 end
